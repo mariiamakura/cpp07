f5abf158268f64f6856eee3e249af1480330dfcd
mariia paraskun <mariiaparaskun@gmail.com>
2023-12-07 15:22:44 +0100

    ready to submit

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,9 @@
 #include "include/Array.hpp"
 
-
 int main( void )
 {
+    Array<int>empty;
+
     Array<int> a(3);
     a[0] = 9;
     a[1] = 8;
@@ -20,10 +21,6 @@ int main( void )
         std::cout << e.what() << std::endl;
     }
 
-    Array<char>cherry(3);
-    cherry[0] = 'a';
-    cherry[1] = 'b';
-    cherry[2] = 'c';
 
     std::cout << "array A : ";
     a.printArr();
@@ -33,8 +30,69 @@ int main( void )
     c.printArr();
     std::cout << std::endl;
 
-    std::cout << "array CHAR : ";
+    Array<std::string>cherry(3);
+    cherry[0] = "aaaaaa";
+    cherry[1] = "bbbbbb";
+    cherry[2] = "cccccc";
+
+
+    std::cout << "array STRING : ";
     cherry.printArr();
     std::cout << std::endl;
     return 0;
 }
+
+//#include <iostream>
+//#include "include/Array.hpp"
+//#include  <cstdlib>
+//
+//#define MAX_VAL 750
+//int main(int, char**)
+//{
+//    Array<int> numbers(MAX_VAL);
+//    int* mirror = new int[MAX_VAL];
+//    srand(time(NULL));
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        const int value = rand();
+//        numbers[i] = value;
+//        mirror[i] = value;
+//    }
+//    //SCOPE
+//    {
+//        Array<int> tmp = numbers;
+//        Array<int> test(tmp);
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        if (mirror[i] != numbers[i])
+//        {
+//            std::cerr << "didn't save the same value!!" << std::endl;
+//            return 1;
+//        }
+//    }
+//    try
+//    {
+//        numbers[-2] = 0;
+//    }
+//    catch(const std::exception& e)
+//    {
+//        std::cerr << e.what() << '\n';
+//    }
+//    try
+//    {
+//        numbers[MAX_VAL] = 0;
+//    }
+//    catch(const std::exception& e)
+//    {
+//        std::cerr << e.what() << '\n';
+//    }
+//
+//    for (int i = 0; i < MAX_VAL; i++)
+//    {
+//        numbers[i] = rand();
+//    }
+//    delete [] mirror;//
+//    return 0;
+//}
